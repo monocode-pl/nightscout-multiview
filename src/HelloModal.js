8c5d07006a7bb50889e270c403d1ba5147ed87a3
psmolenski <psmolenski@moncode.pl>
2023-10-21 19:58:12 +0200

    logo

--- src/HelloModal.js
+++ src/HelloModal.js
@@ -1,11 +1,29 @@
-import {Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text} from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Center, Image,
+    Modal,
+    ModalBody,
+    ModalContent,
+    ModalFooter,
+    ModalHeader,
+    ModalOverlay, Stack,
+    Text
+} from "@chakra-ui/react";
+import logo from './hello-logo.svg';
 
 export function HelloModal({onClose}) {
     return (
         <Modal isOpen={true} onClose={onClose} className='HelloModal' isCentered={true} size={'lg'}>
             <ModalOverlay/>
             <ModalContent>
-                <ModalHeader fontSize={'2xl'}>Witaj w Generatorze Nightscout&nbsp;MultiView</ModalHeader>
+                <ModalHeader fontSize={'2xl'} paddingTop={{base: '80px', md: '110px'}}>
+                    <Stack>
+                        <Image src={logo} maxH={{base: '120px', md: '150px'}} position={'absolute'} top={{base: '-40px', md: '-50px'}} left={'50%'} transform={'translateX(-50%)'} ></Image>
+                        <Box>Witaj w Generatorze Nightscout&nbsp;MultiView</Box>
+                    </Stack>
+
+                </ModalHeader>
                 <ModalBody>
                     <Text mb={5}>
                         Używając Generatora możesz w prosty sposób stworzyć panel pozwalający jednocześnie monitorować wiele
